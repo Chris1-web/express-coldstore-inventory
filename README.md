76acf56b36dffdd76380fe0b307006af46a0788a
Christianah Adeyemo <christianah123.ac@gmail.com>
2023-02-09 09:37:02 +0100

    create Category schema

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # express-coldstore-inventory
+
+mongodb+srv://christianah:mypassword@cluster0.3wwfmha.mongodb.net/coldstore?retryWrites=true&w=majority
---
+++ models/category.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "A name is required"],
+    minLength: 3,
+    maxLength: 100,
+  },
+  description: {
+    type: String,
+    required: [true, "A description is required"],
+    minLength: 100,
+  },
+});
+
+CategorySchema.virtual("url").get(function () {
+  return `/category/${this._id}`;
+});
