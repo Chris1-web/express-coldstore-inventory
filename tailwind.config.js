941193d05345e467e3accf236646af2c743bb578
Christianah Adeyemo <christianah123.ac@gmail.com>
2023-02-19 23:00:38 +0100

    create categories view nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./views/**/*.{pug, html}"],
   theme: {
-    extend: {},
+    extend: {
+      height: (theme) => ({
+        "custom-screen-80": "80vh",
+      }),
+      colors: {
+        "custom-yellow": "#F4EDE8",
+        "custom-blue": "2E2BA8",
+      },
+    },
   },
   plugins: [],
 };
