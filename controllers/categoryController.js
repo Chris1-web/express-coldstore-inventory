ebf7084602c22056b3aa69e4dbb3783ae9352dde
Christianah Adeyemo <christianah123.ac@gmail.com>
2023-02-20 01:41:46 +0100

    create nav mixin for all pages

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,13 @@
+const Category = require("../models/category");
+
 // Displays list of all categories
-exports.category_list = (req, res) => {
-  res.render("categories");
+exports.category_list = (req, res, next) => {
+  Category.find({}).exec(function (err, categories_list) {
+    if (err) return next(err);
+    // Successful, so render
+    console.log(categories_list);
+    res.render("categories", { title: "Categories", categories_list });
+  });
 };
 
 exports.category_create_get = (req, res) => {
