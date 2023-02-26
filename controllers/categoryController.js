fabfe725fb8c3cafdbb53814b7348669d6ff5022
Christianah Adeyemo <christianah123.ac@gmail.com>
2023-02-26 23:15:56 +0100

    create new category in database addede

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const { body, validationResult } = require("express-validator");
 
 // Displays list of all categories
 exports.category_list = (req, res, next) => {
@@ -13,9 +14,49 @@ exports.category_create_get = (req, res) => {
   res.render("category_form", { title: "Add Category" });
 };
 
-exports.category_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Create Category post page");
-};
+exports.category_create_post = [
+  // validate and sanitize
+  body("name", "Category Name is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "Description is required")
+    .trim()
+    .isLength({ min: 100 })
+    .escape(),
+  // process request after validation and santization
+  (req, res, next) => {
+    // extract validation errors from the request
+    const errors = validationResult(req);
+    // // create a category object with escaped and trimmed data
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+    // if there are errors
+    if (!errors.isEmpty()) {
+      res.render("category_form", { title: "Add Category", errors });
+      return;
+    }
+    Category.findOne({ name: req.body.name }).exec((err, found_category) => {
+      if (err) {
+        return next(err);
+      }
+      if (found_category) {
+        // Genre exists, redirect to its detail page.
+        res.redirect(found_category.url);
+      } else {
+        category.save((err) => {
+          if (err) {
+            return next(err);
+          }
+          // Genre saved. Redirect to genre detail page.
+          res.redirect(category.url);
+        });
+      }
+    });
+  },
+];
 
 exports.category_detail = (req, res, next) => {
   const { categoryId } = req.params;
